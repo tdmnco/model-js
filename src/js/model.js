2d89ba31eb664a25663bc437f22d99dec693881e
kaspertidemann <kt@tdmn.co>
2018-05-12 20:25:27 +0200

    v0.0.2

--- src/js/model.js
+++ src/js/model.js
@@ -1,7 +1,32 @@
+// Variables:
+let persist = true
+
+try {
+  localStorage.setItem('1', {})
+} catch(error) {
+  console.warn('Model.js: localStorage not supported!', error)
+
+  persist = false
+}
+
 // Classes:
 class Model {
   constructor(data) {
     Object.assign(this, data)
+
+    this.save()
+  }
+
+  static get(id) {
+    if (persist) {
+      return JSON.parse(localStorage.getItem(this.prototype.constructor.name + '-' + id))
+    }
+  }
+
+  save() {
+    if (persist) {
+      localStorage.setItem(this.constructor.name + '-' + this.id, JSON.stringify(this))
+    }
   }
 }
 
--- test/js/model.test.js
+++ test/js/model.test.js
@@ -5,10 +5,16 @@ import { Model } from '../../src/js'
 class Test extends Model {}
 
 // Tests:
-test('Create a new model', () => {
+test('Create new instance', () => {
   expect(new Test({ id: '1' })).toBeDefined()
 })
 
-test('Get a model id', () => {
+test('Get instance id', () => {
   expect(new Test({ id: '2' }).id).toBeDefined()
+})
+
+test('Get instance from localStorage', () => {
+  new Test({ id: '3' })
+
+  expect(Test.get('3').id).toBe('3')
 })

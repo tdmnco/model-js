9321d11cf025045b3615661ba96e7f7d60657f6e
kaspertidemann <kt@tdmn.co>
2019-02-27 14:47:23 +0100

    Setup function for clearing cache before each test run.

--- test/js/model.test.js
+++ test/js/model.test.js
@@ -9,6 +9,13 @@ class Test3 extends Model {}
 // Prototyping:
 Test2.prototype.modelName = 'Test2'
 
+// Setup:
+afterEach(() => {
+  Test1.deleteCache()
+  Test2.deleteCache()
+  Test3.deleteCache()
+})
+
 // Tests:
 test('Create new instance', () => {
   expect(new Test1({ id: '1' })).toBeDefined()
@@ -21,37 +28,37 @@ test('Cannot create new instance without id', () => {
 })
 
 test('Get instance id', () => {
-  expect(new Test1({ id: '2' }).id).toBe('2')
+  expect(new Test1({ id: '1' }).id).toBe('1')
 })
 
 test('Get instance from localStorage', () => {
-  new Test1({ id: '3' }).save()
+  new Test1({ id: '1' }).save()
 
-  expect(Test1.get('3').id).toBe('3')
+  expect(Test1.get('1').id).toBe('1')
 })
 
 test('Get model name from class', () => {
-  expect(new Test1({ id: '4' }).constructor.name).toBe('Test1')
+  expect(new Test1({ id: '1' }).constructor.name).toBe('Test1')
 })
 
 test('Get model name from modelName property', () => {
-  expect(new Test2({ id: '5' }).modelName).toBe('Test2')
+  expect(new Test2({ id: '1' }).modelName).toBe('Test2')
 })
 
 test('Notify before constructor property update', () => {
-  let test = new Test2({ id: '6' })
+  let test = new Test2({ id: '1' })
 
   test.onbeforeupdate((property, before, after) => {
     expect(property).toBe('id')
-    expect(before).toBe('6')
-    expect(after).toBe('7')
+    expect(before).toBe('1')
+    expect(after).toBe('2')
   })
 
-  test.id = '7'
+  test.id = '2'
 })
 
 test('Notify on runtime property update', () => {
-  let test = new Test1({ id: '7' })
+  let test = new Test1({ id: '1' })
 
   test.firstName = ''
 
@@ -65,11 +72,11 @@ test('Notify on runtime property update', () => {
 })
 
 test('Get instance from cache', () => {
-  let test = new Test1({ id: '8' })
+  let test = new Test1({ id: '1' })
 
   test.save()
 
-  let cachedTest1 = Test1.get('8')
+  let cachedTest1 = Test1.get('1')
 
   test.description = 'Test description 1'
 
@@ -79,76 +86,82 @@ test('Get instance from cache', () => {
 
   test.save()
 
-  let cachedTest2 = Test1.get('8')
+  let cachedTest2 = Test1.get('1')
 
   expect(test.description).toBe(cachedTest1.description)
   expect(test.description).toBe(cachedTest2.description)
 })
 
 test('Get instance property with same name as model function', () => {
-  expect(new Test1({ get: '1', id: '9' }).get).toBe('1')
+  expect(new Test1({ get: '1', id: '1' }).get).toBe('1')
 })
 
 test('Preload instance from localStorage', () => {
-  localStorage.setItem('Test1-10', JSON.stringify({ id: '10' }))
+  localStorage.setItem('Test1-1', JSON.stringify({ id: '1' }))
 
   Test1.preload()
 
-  expect(Test1.cache()['10'].instance.id).toBe('10')
+  expect(Test1.cache()['1'].instance.id).toBe('1')
 })
 
 test('Get instances via property query', () => {
-  new Test1({ company: 'Tidemann&Co', id: '11', ownedBy: 'Kasper Tidemann' }).save()
-  new Test1({ company: 'Alefarm Brewing', id: '12', ownedBy: 'Kasper Tidemann' }).save()
+  new Test1({ company: 'Tidemann&Co', id: '1', ownedBy: 'Kasper Tidemann' }).save()
+  new Test1({ company: 'Alefarm Brewing', id: '2', ownedBy: 'Kasper Tidemann' }).save()
 
   expect(Test1.get({ ownedBy: 'Kasper Tidemann' }).length).toBe(2)
 })
 
 test('Get instances via array of ids', () => {
-  new Test1({ id: '13' }).save()
-  new Test1({ id: '14' }).save()
-  new Test1({ id: '15' }).save()
-  new Test1({ id: '16' }).save()
+  new Test1({ id: '1' }).save()
+  new Test1({ id: '2' }).save()
+  new Test1({ id: '3' }).save()
+  new Test1({ id: '4' }).save()
 
-  expect(Test1.get(['14', '15', '16']).length).toBe(3)
+  expect(Test1.get(['2', '3', '4']).length).toBe(3)
 })
 
 test('Get all instances', () => {
-  expect(Test1.get().length).toBe(9)
+  new Test1({ id: '1' }).save()
+  new Test1({ id: '2' }).save()
+  new Test1({ id: '3' }).save()
+  new Test1({ id: '4' }).save()
+  new Test1({ id: '5' }).save()
+
+  expect(Test1.get().length).toBe(5)
 })
 
 test('Get first instance via property query', () => {
-  let test = new Test1({ bandName: 'Metallica', id: '17' })
+  let test = new Test1({ bandName: 'Metallica', id: '1' })
 
   test.save()
 
-  expect(Test1.first({ bandName: 'Metallica' }).id).toBe('17')
+  expect(Test1.first({ bandName: 'Metallica' }).id).toBe('1')
 })
 
 test('Get first instance via array of ids', () => {
-  new Test1({ id: '18' }).save()
-  new Test1({ id: '19' }).save()
-  new Test1({ id: '20' }).save()
+  new Test1({ id: '1' }).save()
+  new Test1({ id: '2' }).save()
+  new Test1({ id: '3' }).save()
 
-  expect(Test1.first(['19']).id).toBe('19')
+  expect(Test1.first(['1']).id).toBe('1')
 })
 
 test('Delete instance', () => {
-  let test = new Test1({ id: '21' })
+  let test = new Test1({ id: '1' })
 
   test.save()
 
   test.delete()
 
-  expect(Test1.get('21')).toBe(null)
+  expect(Test1.get('1')).toBe(null)
 })
 
 test('Get caches for three separate models', () => {
-  new Test1({ id: '23' }).save()
-  new Test2({ id: '24' }).save()
-  new Test3({ id: '25' }).save()
+  new Test1({ id: '1' }).save()
+  new Test2({ id: '2' }).save()
+  new Test3({ id: '3' }).save()
 
-  expect(Object.keys(Test1.cache()).length).toBe(14)
+  expect(Object.keys(Test1.cache()).length).toBe(1)
   expect(Object.keys(Test2.cache()).length).toBe(1)
   expect(Object.keys(Test3.cache()).length).toBe(1)
 })
@@ -166,7 +179,7 @@ test('Delete caches for three separate models', () => {
 test('Notify before instance deletion', () => {
   const callback = jest.fn()
 
-  let test = new Test2({ id: '26' })
+  let test = new Test2({ id: '1' })
 
   test.onbeforedelete(callback)
 
@@ -177,8 +190,8 @@ test('Notify before instance deletion', () => {
 
 test('JSON stringify comparison', () => {
   const now = new Date().toISOString()
-  const test = new Test3({ created: now, firstname: 'Kasper', id: '27', lastname: 'Tidemann' })
-  const json = { created: now, firstname: 'Kasper', id: '27', lastname: 'Tidemann' }
+  const test = new Test3({ created: now, firstname: 'Kasper', id: '1', lastname: 'Tidemann' })
+  const json = { created: now, firstname: 'Kasper', id: '1', lastname: 'Tidemann' }
 
   expect(Object.keys(json)).toEqual(Object.keys(test))
   expect(JSON.stringify(test)).toEqual(JSON.stringify(json))
